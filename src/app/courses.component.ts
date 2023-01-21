f9877ef0b03d839b8a04bea6a872545922a18202
Be-Asia McKerracher <amerigirl29@gmail.com>
2023-01-20 22:58:01 -0600

    working through the components

---
+++ src/app/courses.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'courses', //<everywhere it goes, so does the courses component>
+    template: '<h2>Courses</h2>'
+
+})
+
+export class CoursesComponent {
+
+}
