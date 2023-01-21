06584f8f87720044a549b6491e7c4e17b34bdeee
Be-Asia McKerracher <amerigirl29@gmail.com>
2023-01-20 23:14:20 -0600

    pushing through

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({        //converts a typescript class to a module from angular's POV
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses', //<everywhere it goes, so does the courses component>
-    template: '<h2>Courses</h2>'
+    template: '<h2>{{ getTitle() }}</h2>'
 
 })
 
-export class CoursesComponent {
+ export class CoursesComponent {
+    title = "List of Courses";
+
+
+
+
+getTitle(){
+    return this.title;
+}
 
 }
